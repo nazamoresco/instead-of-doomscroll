e4c189f57934c37aeefe118fb7638d972cd00da3
nazamoresco <nazamoresco99@gmail.com>
2025-10-05 18:30:15 -0300

    move logo to top-left and smaller

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import Image from "next/image";
 
 export function Logo() {
-  return <Image src="/logo.svg" alt="Logo" width={640} height={510} priority />;
+  return <Image src="/logo.svg" alt="Logo" width={128} height={128} priority />;
 }
   
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <main className="flex flex-col items-center">
-        <Logo />
+        <div className="absolute top-0 left-0 m-4">
+          <Logo />
+        </div>
         <SuggestionForm />
         <SuggestionList />
       </main>
