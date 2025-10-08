2662bcfe976fb8229441f17d72355ee292a5cfca
nazamoresco <nazamoresco99@gmail.com>
2025-10-08 19:22:02 -0300

    make suggestion list readable by everyone but writable scoped to each's ones suggestions

--- src/app/lib/createSingletonSuggestionList.ts
+++ src/app/lib/createSingletonSuggestionList.ts
@@ -1,6 +1,11 @@
 import { SuggestionList } from '../schema';
+import { Group } from 'jazz-tools';
 
 export const createSingletonSuggestionList = () => {
-  const newList = SuggestionList.create([]);
+  const group = Group.create();
+  group.addMember("everyone", "writeOnly");
+  group.makePublic("reader");
+
+  const newList = SuggestionList.create([], group);
   console.log("New SuggestionList ID:", newList.$jazz.id);
 };
