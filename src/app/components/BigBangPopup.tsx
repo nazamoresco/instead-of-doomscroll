fcc366f95c6203d136ad2e34987018c41faba3f3
nazamoresco <nazamoresco99@gmail.com>
2025-10-08 19:16:43 -0300

    create big bang button

--- src/app/components/BigBangPopup.tsx
+++ src/app/components/BigBangPopup.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useAccount } from "jazz-tools/react";
 import { Account } from "../schema";
+import { createSingletonSuggestionList } from "../lib/createSingletonSuggestionList";
 
 interface BigBangPopupProps {
   onHide: () => void;
@@ -12,10 +13,24 @@ export function BigBangPopup({ onHide, suggestionListIdSet }: BigBangPopupProps)
   const isAdmin = !!me;
   const canHide = isAdmin && !suggestionListIdSet;
 
+  const handleBigBang = () => {
+    createSingletonSuggestionList();
+
+    if (canHide) {
+      onHide();
+    }
+  };
+
   return (
     <div className="fixed top-10 left-10 bg-yellow-200 dark:bg-yellow-800 p-4 rounded-lg shadow-lg z-20">
       <h2 className="text-xl font-bold">Big Bang</h2>
       <p>Create initial Suggestion List</p>
+      <button
+        onClick={handleBigBang}
+        className="mt-2 bg-blue-500 text-white p-2 rounded mr-2"
+      >
+        Big Bang!
+      </button>
       {canHide && (
         <button
           onClick={onHide}
