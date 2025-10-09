8683cd08ca933d8ab499c927b1b8376e12e37a33
nazamoresco <nazamoresco99@gmail.com>
2025-10-09 17:18:16 -0300

    fix suggestion feed reference

--- src/app/lib/createSingletonSuggestionList.ts
+++ src/app/lib/createSingletonSuggestionList.ts
@@ -1,11 +1,11 @@
-import { SuggestionList } from '../schema';
+import { SuggestionFeed } from '../schema';
 import { Group } from 'jazz-tools';
 
 export const createSingletonSuggestionList = () => {
   const group = Group.create();
   group.addMember("everyone", "writer");
 
-  const newList = SuggestionList.create([], group);
-  console.log("New SuggestionList ID:", newList.$jazz.id);
+  const newList = SuggestionFeed.create([], group);
+  console.log("New SuggestionFeed ID:", newList.$jazz.id);
   process.env.NEXT_PUBLIC_SUGGESTION_LIST_ID = newList.$jazz.id;
 };
